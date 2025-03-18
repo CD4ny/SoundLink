e396b044a258b91cca92046655bf528c93d0155f
cd4ny <chavianodaniel99@gmail.com>
2025-03-18 02:05:01 -0400

    feat: create sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { Route, Routes } from "react-router";
+import Main from "./pages/main";
+import Login from "./pages/login";
+import List from "./pages/list";
+import Account from "./pages/ account";
+
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="login" element={<Login />} />
+            <Route path="account" element={<List/>}>
+                <Route path=":id" element={<Account/>}/>
+            </Route>
+        </Routes>
+    )
 }
